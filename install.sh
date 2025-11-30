08c993183bbb11e4c6771f77b099d1a8cf8a9579
aday54 <123194708+aday54@users.noreply.github.com>
2025-11-30 02:53:41 -0700

    Attempt combine (still not working)

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-clear
-#Sets the time
-read -p "Enter Country " country
-read -p "Enter City" city
-exec timedatectl set-timezone $country / $city
+exec timedatectl
 
 clear
 #Partitions the disks
-echo "Find your storage device and remember the name of it"
 lsblk
-read -p "Enter the storage device name" storage
+read -p "Enter the storage device name you'd like to use " storage
 echo "Configuring partition scheme (swap will not work for this script)"
 read
 cfdisk /dev/$storage
@@ -33,13 +28,13 @@ clear
 sudo pacman -Sy archlinux-keyring 
 pacstrap /mnt base base-devel linux linux-firmware vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash 
+arch-chroot /mnt /bin/bash <<EOF
 
 clear
 pacman -Syu
 pacman -S networkmanager grub ufw
 systemctl enable NetworkManager.service
-read -p "Enter the storage device name" storage
+read -p "Enter the storage device name " storage
 grub-install --target=i386-pc /dev/$storage #Change if not using intel
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable ufw
@@ -50,10 +45,23 @@ read
 nano /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-read-p "enter a name you want for your system" hostname
+read-p "enter a name you want for your system " hostname
 echo $hostname >> /etc/hostname
 
 clear
 ls /usr/share/zoneinfo/America/
 read -p "Pick the closest city to you on this list" city
 ln -sf /usr/share/zoneinfo/America/$city /etc/localtime
+
+clear
+#Sets the time
+read -p "Enter Country " country
+read -p "Enter City" city
+exec timedatectl set-timezone $country / $city
+
+EOF
+
+echo "Installation theoretically complete"
+read
+
+reboot

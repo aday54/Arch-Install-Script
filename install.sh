e9dc609fa93a8376063ba60ccd754706a20c6013
aday54 <123194708+aday54@users.noreply.github.com>
2025-12-02 15:47:03 -0700

    Made the script functional

--- install.sh
+++ install.sh
@@ -1,67 +1,66 @@
 #!/bin/bash
-
-exec timedatectl
-
+timedatectl
 clear
 #Partitions the disks
 lsblk
-read -p "Enter the storage device name you'd like to use " storage
+read -p "Enter the storage device name you'd like to use: " storage
 echo "Configuring partition scheme (swap will not work for this script)"
 read
 cfdisk /dev/$storage
-
 clear
 #Formats the partitions
 dev1="${storage}1"
 dev2="${storage}2"
 mkfs.ext4 /dev/$dev1
 mkfs.ext4 /dev/$dev2
-
 clear
 #Mounts the disks 
+mount /dev/$dev2 /mnt
 mkdir /mnt/boot
 mount /dev/$dev1 /mnt/boot
-mount /dev/$dev2 /mnt
-
 clear
 #Installs the Operating System and enter the new system with chroot
-sudo pacman -Sy archlinux-keyring 
-pacstrap /mnt base base-devel linux linux-firmware vim nano
+sudo pacman -Sy archlinux-keyring --noconfirm
+pacstrap /mnt base base-devel linux linux-firmware vim nano --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash <<EOF
-
 clear
-pacman -Syu
-pacman -S networkmanager grub ufw
+echo "You are now chrooted into the system"
+read </dev/tty
+pacman -Syu --noconfirm
+pacman -S --noconfirm networkmanager grub ufw
 systemctl enable NetworkManager.service
-read -p "Enter the storage device name " storage
-grub-install --target=i386-pc /dev/$storage #Change if not using intel
+lsblk
+read -p "Enter the storage device name, not it's partitions (for example sda, not sda1): " storage </dev/tty
+grub-install --target=i386-pc /dev/\$storage #Change if not using Bios
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable ufw
-
 clear
-echo "Uncomment the UTF text for your country and save/exit. Make sure not to be doing it in the example section"
-read
-nano /etc/locale.gen
+read -p "The default Locale generated will be en_US.UTF-8, would you like to do something else? (y/n) " localebool
+default="en_US.UTF-8"
+if [ "$localebool" = "y" ]; then
+    read -p "Enter other locale (e.g. en_GB.UTF-8): " locale
+    sed -i "s|^#\(${locale}.*\)|\1|" /etc/locale.gen
+else
+    locale="$default"
+    sed -i "s|^#\(${locale}.*\)|\1|" /etc/locale.gen
+fi
+# Generate the locale
+locale-gen
+echo "LANG=$locale" > /etc/locale.conf
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-read-p "enter a name you want for your system " hostname
-echo $hostname >> /etc/hostname
-
+clear
+read -p "Enter a name you want for your system: " hostname </dev/tty
+echo \$hostname >> /etc/hostname
 clear
 ls /usr/share/zoneinfo/America/
-read -p "Pick the closest city to you on this list" city
-ln -sf /usr/share/zoneinfo/America/$city /etc/localtime
-
+read -p "Pick the closest city to you on this list: " city </dev/tty
+ln -sf /usr/share/zoneinfo/America/\$city /etc/localtime
 clear
-#Sets the time
-read -p "Enter Country " country
-read -p "Enter City" city
-exec timedatectl set-timezone $country / $city
-
+echo "please enter a root password"
+passwd </dev/tty
+clear
 EOF
-
-echo "Installation theoretically complete"
+echo "Installation complete, you may now reboot the system"
 read
-
-reboot
